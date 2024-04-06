575e8c89d3ff5def3ba2385ea5d8314619586f54
Hel <hdsanchez1@student.hau.edu.ph>
2024-04-06 13:45:21 +0800

    2nd commit

--- projectSample123/urls.py
+++ projectSample123/urls.py
@@ -16,9 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from projectSample123.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home"),
     path('jacobi/', include('jacobi.urls')),
     path('cramers/', include('cramers.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
